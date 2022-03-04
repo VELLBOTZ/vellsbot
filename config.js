aa9b5e8e500aca6011e5fe6e675d03b1dffd76d7
Alwan-Karsa <100394658+Alwan-Karsa@users.noreply.github.com>
2022-03-04 17:44:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -48,8 +48,8 @@ global.wait = '「 ⏱️ 」Harap tunggu...'
 global.eror = '「❗」Server ERROR!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.watermark = '© Alwan Karsa' //change the watermark 
-global.image = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg' //change the image
-global.thumbfoto = 'https://telegra.ph/file/74ee3213700db6534e59a.png'
+global.image = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBzu20tdAFqElapjLJvTTftQDyzDXNTfnJXQ&usqp=CAU' //change the image
+global.thumbfoto = 'https://w0.peakpx.com/wallpaper/181/22/HD-wallpaper-hxh-alluka-cat-girl.jpg'
 
 // image
 global.bank = 'https://telegra.ph/file/d5ddf4cc627bb0e6bc420.jpg'
